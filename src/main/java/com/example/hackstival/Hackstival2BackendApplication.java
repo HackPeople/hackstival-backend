ffe5c25b8170770c26f21c14a09051721ccfb42b
ju-ong <young290@lguplus.co.kr>
2023-08-31 00:45:49 +0900

    chore: first commit

---
+++ src/main/java/com/example/hackstival/Hackstival2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackstival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackstival2BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hackstival2BackendApplication.class, args);
+    }
+
+}
