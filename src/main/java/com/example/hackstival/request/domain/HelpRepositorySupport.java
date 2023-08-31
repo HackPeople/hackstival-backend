6ff616734e6de1b3f33d123e6abfa1a16bece782
ju-ong <young290@lguplus.co.kr>
2023-08-31 21:44:56 +0900

    fix: null error

--- src/main/java/com/example/hackstival/request/domain/HelpRepositorySupport.java
+++ src/main/java/com/example/hackstival/request/domain/HelpRepositorySupport.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.example.hackstival.common.Constant.parseFullDateTime;
@@ -19,6 +20,7 @@ import static com.example.hackstival.request.domain.QHelp.help;
 public class HelpRepositorySupport extends QuerydslRepositorySupport {
 
     private final JPAQueryFactory jpaQueryFactory;
+
     public HelpRepositorySupport(JPAQueryFactory jpaQueryFactory) {
         super(Help.class);
         this.jpaQueryFactory = jpaQueryFactory;
@@ -30,8 +32,8 @@ public class HelpRepositorySupport extends QuerydslRepositorySupport {
                 .select(help)
                 .from(help)
                 .where(isRequestStatus())
-                .where(afterStartTime(parseFullDateTime(searchCondition.getStartTime())))
-                .where(beforeEndTime(parseFullDateTime(searchCondition.getEndTime())))
+                .where(afterStartTime(searchCondition.getStartTime()))
+                .where(beforeEndTime(searchCondition.getEndTime()))
                 .where(inRequestType(searchCondition.getRequestTypes()))
                 .where(eqStartDongAddress(searchCondition.getDongName()))
                 .orderBy(createOrderSpecifier(searchCondition.getSortType()))
@@ -42,16 +44,17 @@ public class HelpRepositorySupport extends QuerydslRepositorySupport {
         return help.requestStatus.eq(RequestStatus.REQUEST);
     }
 
-    private BooleanExpression afterStartTime(LocalDateTime time){
-        return time == null ? null : help.startTime.after(time);
+    private BooleanExpression afterStartTime(String time) {
+        return time == null ? null : help.startTime.after(parseFullDateTime(time));
     }
 
-    private BooleanExpression beforeEndTime(LocalDateTime time) {
-        return time == null ? null : help.endTime.before(time);
+    private BooleanExpression beforeEndTime(String time) {
+        return time == null ? null : help.endTime.before(parseFullDateTime(time));
 
     }
 
     private BooleanExpression inRequestType(List<RequestType> requestTypes) {
+        if (requestTypes == null) return null;
         return requestTypes.isEmpty() ? null : help.requestType.in(requestTypes);
 
     }
@@ -61,10 +64,14 @@ public class HelpRepositorySupport extends QuerydslRepositorySupport {
     }
 
 
-    private OrderSpecifier createOrderSpecifier(SortType sortType) {
-        if(sortType == SortType.MONEY_DESC) return new OrderSpecifier(Order.DESC, help.money);
-        return null;
-    }
+    private OrderSpecifier[] createOrderSpecifier(SortType sortType) {
+        List<OrderSpecifier> orderSpecifiers = new ArrayList<>();
 
+        if (sortType == SortType.MONEY_DESC) orderSpecifiers.add(new OrderSpecifier(Order.DESC, help.money));
+
+        orderSpecifiers.add(new OrderSpecifier(Order.DESC, help.id));
+
+        return orderSpecifiers.toArray(new OrderSpecifier[orderSpecifiers.size()]);
+    }
 
 }
